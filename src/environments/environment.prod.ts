7bae2d94fed55c1e8a12282a5c7684f3135ce094
EricRCorral <eric-wil@hotmail.com>
2020-02-21 18:10:48 -0300

    Uploading deployable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBiAxWBTmSjbr9mHfmbGR23IpyMJ7ZKPYw',
+    authDomain: 'fir-fotos-3682b.firebaseapp.com',
+    databaseURL: 'https://fir-fotos-3682b.firebaseio.com',
+    projectId: 'fir-fotos-3682b',
+    storageBucket: 'fir-fotos-3682b.appspot.com',
+    messagingSenderId: '221170857631',
+    appId: '1:221170857631:web:48101b1181619e6d8a94aa',
+    measurementId: 'G-RW3NGVBX9Y'
+  }
 };
